4dde33734e4930a7d9385ebacff4489b63248add
mdshahbajalam7 <98772205+mdshahbajalam7@users.noreply.github.com>
2023-01-03 21:11:16 +0530

    pagination done

--- client-f/src/App.js
+++ client-f/src/App.js
@@ -1,8 +1,9 @@
 import Container from "@mui/material/Container";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Auth from "./components/Auth/Auth";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
+import PostDetails from "./components/PostDetails/PostDetails";
 // import {witch} from 'react-router-dom'
 // import { Route, Switch } from 'react-router-dom'
 
@@ -10,13 +11,19 @@ import Navbar from "./components/Navbar/Navbar";
 //   "https://raw.githubusercontent.com/adrianhajdin/project_mern_memories/master/client/src/images/memories.png?token=AF56X74XONEUGZ4FD2FUIA27UURPI";
 
 function App() {
+  // const user = JSON.parse(localStorage.getItem(""))
   return (
-    <Container maxWidth="lg">
+    <Container maxWidth="xl">
       <Navbar />
+      {/* <Switch> */}
       <Routes>
+        {/* <Route path="/" exact element={() => <Navigate to="/posts" replace/>} /> */}
         <Route path="/" exact element={<Home />} />
+        <Route path="/search" exact element={<Home />} />
+        <Route path="/:id" element={<PostDetails />} />
         <Route path="/auth" exact element={<Auth />} />
       </Routes>
+      {/* </Switch> */}
     </Container>
   );
 }
--- client-f/src/components/Home/Home.jsx
+++ client-f/src/components/Home/Home.jsx
@@ -1,8 +1,9 @@
-import { Container, Grid, Grow } from "@material-ui/core";
+import { Container, Grid, Grow, Paper } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { getpost } from "../../actions/posts";
 import Form from "../Form/Form";
+import Paginations from "../Pagination/Paginations";
 import Posts from "../Posts/Posts";
 // import useStyles from "./styles";
 function Home() {
@@ -28,6 +29,9 @@ function Home() {
           </Grid>
           <Grid item xs={12} sm={4}>
             <Form currentId={currentId} setcurrentId={setcurrentId} />
+            <Paper elevation={6}>
+              <Paginations/>
+            </Paper>
           </Grid>
         </Grid>
       </Container>
